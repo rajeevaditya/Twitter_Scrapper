09560eaaf3e60a807c54ef2113678bccad5dcf3c
rajeevaditya <126942001+rajeevaditya@users.noreply.github.com>
2023-04-18 02:39:48 +0530

    v2
    v2

--- Twitter_Scraper.py
+++ Twitter_Scraper.py
@@ -5,16 +5,35 @@ import datetime
 import pymongo
 import time
 
+st.set_page_config(page_icon="logo.png", page_title="Twitter Data Scrapper")
+
 # REQUIRED VARIABLES
 client = pymongo.MongoClient("mongodb://localhost:27017")  # To connect to MONGODB
 mydb = client["Twitter_Database"]    # To create a DATABASE
 
 tweets_df = pd.DataFrame()
-st.write("# Twitter Data scraping")
-option = st.selectbox('How would you like the data to be searched?',('Keyword', 'Hashtag'))
-word = st.text_input('Please enter a '+option, 'RRR')
-start = st.date_input("Select the start date", datetime.date(2022, 1, 1),key='d1')
-end = st.date_input("Select the end date", datetime.date(2023, 1, 1),key='d2')
+a, b = st.columns([1, 5])
+
+with a:
+    st.text("")
+    st.image("logo.png", width=50)
+with b:
+    st.title("Twitter Data scraping")
+st.write("Type in a term to scrape relevant Tweets.")
+w,x = st.columns(2)
+with w:
+    option = st.selectbox('How would you like the data to be searched?',('Keyword', 'Hashtag'))
+with x:
+    word = st.text_input('Please enter a '+option, 'RRR')
+
+y,z = st.columns(2)
+with y:
+    start = st.date_input("Select the start date", datetime.date(2022, 1, 1),key='d1')
+with z:
+    end = st.date_input("Select the end date", datetime.date(2023, 1, 1),key='d2')
+
+
+
 tweet_c = st.slider('How many tweets to scrape', 0, 1000, 5)
 tweets_list = []
 
@@ -24,21 +43,33 @@ if word:
         for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} + since:{start} until:{end}').get_items()):
             if i>=tweet_c:
                 break
-            tweets_list.append([ tweet.id, tweet.date,  tweet.content, tweet.lang, tweet.user.username, tweet.replyCount, tweet.retweetCount,tweet.likeCount, tweet.source, tweet.url ])
-        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username', 'ReplyCount', 'RetweetCount', 'LikeCount','Source', 'Url'])
+            tweets_list.append([ tweet.id, tweet.date,  tweet.rawContent, tweet.lang, tweet.user.username,
+                                tweet.user.followersCount, tweet.user.friendsCount, tweet.replyCount,tweet.retweetCount,tweet.quoteCount, 
+                                tweet.likeCount,tweet.hashtags, tweet.sourceUrl, tweet.url,tweet.media ])
+        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username','followersCount', 'friendsCount', 'ReplyCount', 'RetweetCount', 'quoteCount', 'LikeCount', 'hashtags' ,'Source', 'Url','Media'])
     else:
         for i,tweet in enumerate(sntwitter.TwitterHashtagScraper(f'{word} + since:{start} until:{end}').get_items()):
             if i>tweet_c:
-                break            
-            tweets_list.append([ tweet.id, tweet.date,  tweet.content, tweet.lang, tweet.user.username, tweet.replyCount, tweet.retweetCount,tweet.likeCount, tweet.source, tweet.url ])
-        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username', 'ReplyCount', 'RetweetCount', 'LikeCount','Source', 'Url'])
+                break
+            tweets_list.append([ tweet.id, tweet.date,  tweet.rawContent, tweet.lang, tweet.user.username,
+                                tweet.user.followersCount, tweet.user.friendsCount, tweet.replyCount,tweet.retweetCount,tweet.quoteCount, 
+                                tweet.likeCount,tweet.hashtags, tweet.sourceUrl, tweet.url,tweet.media ])
+        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username','followersCount', 'friendsCount', 'ReplyCount', 'RetweetCount', 'quoteCount', 'LikeCount', 'hashtags' ,'Source', 'Url','Media'])
 else:
     st.warning(option,' cant be empty', icon="⚠️")
+    e = RuntimeError('This is an exception of type RuntimeError')
+    st.exception(e) 
 
 # DOWNLOAD AS CSV
 @st.cache # IMPORTANT: Cache the conversion to prevent computation on every rerun
 def convert_df(df):    
     return df.to_csv().encode('utf-8')
+
+def progress_bar():
+    my_bar = st.progress(0)
+    for percent_complete in range(100):
+        time.sleep(0.1)
+        my_bar.progress(percent_complete + 1)
 
 
 if not tweets_df.empty:
@@ -57,12 +88,18 @@ if not tweets_df.empty:
         z=st.button('Upload Tweets to Database',key=3)        
 
 if c:
-    st.success("The Scraped Data is Downloaded as .CSV file:",icon="✅")  
+    progress_bar()
+    st.success("The Scraped Data is Downloaded as .CSV file:",icon="✅")
+    st.snow()  
 if j:
-    st.success("The Scraped Data is Downloaded as .JSON file",icon="✅")     
+    progress_bar()
+    st.success("The Scraped Data is Downloaded as .JSON file",icon="✅")
+    st.snow()     
 
 if y: # DISPLAY
+    progress_bar()
     st.success("Tweets Scraped Successfully:",icon="✅")
+    st.snow()
     st.write(tweets_df)
 
 if z: # upload to DB
@@ -79,9 +116,5 @@ if z: # upload to DB
             st.snow()
         else:
             st.warning('Cant upload because there are no tweets', icon="⚠️")
-
-    
-
-
-            
-
+            e = RuntimeError('This is an exception of type RuntimeError')
+            st.exception(e) 
